a7ffdf8f086c8f9ec0cb9d8ac7540da61eb7d56f
Satwik Ram <satwikram29@gmail.com>
2025-07-08 21:35:34 -0500

    update

--- llm.py
+++ llm.py
@@ -13,3 +13,14 @@ class MultiAgent:
         self.model = os.environ.get("MODEL")
 
         self.solver_prompt = solver_prompt
+        self.solver_json_schema = None
+
+        #Initiate Solver LLM
+        self.solver_llm = ChatOpenAI(model=self.model, temperature=0)
+        self.solver_llm.with_structured_output(self.solver_json_schema)
+
+    def get_solver_response(self, ui_tree, command=None):
+
+        
+        
+
