d96c9c215eb9faaf8fd6e3b6a5d0e926a0616d10
Satwik Ram <satwikram29@gmail.com>
2025-07-09 16:50:37 -0400

    updated code

--- llm.py
+++ llm.py
@@ -1,11 +1,19 @@
 from langchain.chat_models import init_chat_model
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, BaseMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
 
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from typing import Sequence
+from langgraph.graph.message import add_messages
+from typing_extensions import Annotated, TypedDict
 
 import os
+from agents.solver.prompt import *
+
+class State(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    ui_tree: str
 
 class MultiAgent:
 
@@ -16,7 +24,7 @@ class MultiAgent:
         self.solver_llm = init_chat_model(self.model_name, model_provider="openai")
 
         # Define a new state graph
-        self.workflow = StateGraph(state_schema=MessagesState)
+        self.workflow = StateGraph(state_schema=State)
 
         # Define the (single) node in the graph
         self.workflow.add_edge(START, "model")
@@ -26,16 +34,27 @@ class MultiAgent:
         memory = MemorySaver()
         self.app = self.workflow.compile(checkpointer=memory)
 
+        self.prompt_template = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                solver_prompt,
+            ),
+            MessagesPlaceholder(variable_name="messages"),
+        ])
+
+
     # Define the function that calls the model
     def call_model(self, state: MessagesState):
-        response = self.solver_llm.invoke(state["messages"])
-        return {"messages": response}
+        prompt = self.prompt_template.invoke(state)
+        response = self.solver_llm.invoke(prompt)
+        return {"messages": [response]}
     
-    def get_solver_response(self, query, config):
+    def get_solver_response(self, query, ui_tree, config):
 
         input_messages = [HumanMessage(query)]
-        output = self.app.invoke({"messages": input_messages}, config)
-        
+        output = self.app.invoke({"messages": input_messages, "ui_tree": ui_tree}, config)
+
         return output["messages"][-1] # output contains all messages in state
 
 
