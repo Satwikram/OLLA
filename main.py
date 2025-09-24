e01cfa12a256ef76d40e2b26c94840dca4af9a98
Satwik Ram <satwikram29@gmail.com>
2025-09-23 20:15:06 -0400

    update

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def predict_action(query):
         print("Did not find any relevant control element")
 
 # query = "Task: Change Margins to Narrow"
-query = "Task: Change the font size to 14.5"
+query = "Task: Change the font size to 10"
 # query = "Task: Add a new comment with text 'Check this section'"
 # query = "Task: Center the alignment for the text"
 # query = "Task: Change the mode to Reviewing"
