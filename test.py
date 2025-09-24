3d8159df85640182255ca497f1ed25813f885aeb
Satwik Ram <satwikram29@gmail.com>
2025-09-23 20:01:34 -0400

    update

--- test.py
+++ test.py
@@ -3,13 +3,16 @@ from ui_automation.ui_manager import *
 
 obj = UIManager()
 
-# element = {
+element = {
     
-#     "title": "Aptos (Body)",
-#     "control_type": "Edit",
-# }
+    "title": "12",
+    "control_type": "Edit",
+    "value": 14.5,
+}
 
-print(obj.get_ui_tree())
+# print(obj.get_ui_tree())
 
 # property = obj.get_control_properties(element)
 # print(property)
+
+obj.simulate(element)
