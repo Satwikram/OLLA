76c94718ea74294363ba8f39767fea5373e8b988
Satwik Ram <satwikram29@gmail.com>
2025-10-18 11:21:36 -0400

    test file

--- test.py
+++ test.py
@@ -1,60 +1,16 @@
-from ui_automation.ui_manager import *
+from ui_automation.ui_manager import UIManager
 
-
-# obj = UIManager()
-
-element = {
-    
-    "title": "12",
-    "control_type": "Edit",
-    "value": 14.5,
-}
-
-
-# print(obj.get_ui_tree())
-
-# property = obj.get_control_properties(element)
-# print(property)
-
-# obj.simulate(element)
-
-
-import pygetwindow as gw
-from pywinauto.application import Application
-from io import StringIO
-from contextlib import redirect_stdout
-import pyttsx3
-from utils import *
-from pywinauto.uia_defines import IUIA  
-from pywinauto.controls.uiawrapper import UIAWrapper
-
-
-class UIManager:
-
-    def __init__(self):
-
-        self.util = Utils()
-
-        self.active_window = gw.getActiveWindow()
-        self.app = Application(backend="uia").connect(title=self.active_window.title, visible_only=False)
-        self.window = self.app.window(title=self.active_window.title)
-
-    def get_ui_tree(self):
-        """
-        Retrieves the UI tree of the currently active window.
-        """
-        dialog = self.app.top_window()  
-        dialog.set_focus()
-        print(dir(dialog))
-
-        output = StringIO()
-
-        with redirect_stdout(output):
-            dialog.print_control_identifiers()
-
-        ui_tree = output.getvalue()
-
-        return ui_tree
-    
 obj = UIManager()
-obj.get_ui_tree()
+# element_data = {
+
+#     "title": "Aptos (Body)",
+#     "control_type": "Edit",
+# }
+# obj.simulate(element_data)
+
+obj.get_screenshot()
+
+tree = obj.get_ui_tree()
+
+with open("ui_tree.txt", "w+", encoding="utf-8") as f:
+    f.write(tree)
