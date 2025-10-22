87257d80d08738e589ff46fdf89d0549dea86936
Satwik Ram <satwikram29@gmail.com>
2025-10-22 14:39:03 -0400

    updated code

--- test.py
+++ test.py
@@ -1,16 +1,30 @@
-from ui_automation.ui_manager import UIManager
+# from ui_automation.ui_manager import UIManager
 
-obj = UIManager()
-# element_data = {
+# obj = UIManager()
+# # element_data = {
 
-#     "title": "Aptos (Body)",
-#     "control_type": "Edit",
-# }
-# obj.simulate(element_data)
+# #     "title": "Aptos (Body)",
+# #     "control_type": "Edit",
+# # }
+# # obj.simulate(element_data)
 
-obj.get_screenshot()
+# obj.get_screenshot()
 
-tree = obj.get_ui_tree()
+# tree = obj.get_ui_tree()
 
-with open("ui_tree.txt", "w+", encoding="utf-8") as f:
-    f.write(tree)
+# with open("ui_tree.txt", "w+", encoding="utf-8") as f:
+#     f.write(tree)
+
+from pynput import keyboard
+
+def on_press(key):
+    print("press:", key, "vk:", getattr(key, "vk", None), "name:", getattr(key, "name", None))
+
+def on_release(key):
+    print("release:", key, "vk:", getattr(key, "vk", None))
+    if key == keyboard.Key.esc:
+        return False
+
+print("Press F9 (or Fn+F9 on some laptops). Press ESC to quit.")
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
